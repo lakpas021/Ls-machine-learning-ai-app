8139e7d2c2f1f807393e3150ea8c7023dccff54b
Lakprashant Sherpa <lakpas021@gmail.com>
2025-02-20 14:58:00 +0900

    Update streamlit_app.py
    added name signature on the topi

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,9 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-st.title('🤖 Machine Learning App
-          /n by Lakpa')
+st.title('🤖 Machine Learning App')
 
-st.info('This is app builds a machine learning model!')
+st.info('This is app builds a machine learning model buid and trained by Lakpa Sherpa!')
 
 with st.expander('Data'):
   st.write('**Raw data**')
