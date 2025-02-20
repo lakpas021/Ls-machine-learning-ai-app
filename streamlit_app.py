4cb417736dc0f8d524f934c6022df1142d854318
Lakprashant Sherpa <lakpas021@gmail.com>
2025-02-20 14:56:15 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-st.title('🤖 Machine Learning App')
+st.title('🤖 Machine Learning App
+          /n by Lakpa')
 
 st.info('This is app builds a machine learning model!')
 
